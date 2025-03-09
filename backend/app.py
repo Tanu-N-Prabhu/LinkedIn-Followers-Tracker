2b2e2b2e2048e7d81950377b40fc2fc4a4de8d0c
Tanu-N-Prabhu <tanunprabhu95@gmail.com>
2025-03-08 23:38:00 -0600

    Added a new file

--- backend/app.py
+++ backend/app.py
@@ -102,8 +102,7 @@ def forecast_followers():
 
     return jsonify(forecast_results)
 
+from waitress import serve
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Use Railway’s assigned PORT or fallback to 5000
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=8000)
